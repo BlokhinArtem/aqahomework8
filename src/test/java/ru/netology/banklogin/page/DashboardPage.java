82be11560a354f6b4214ef91a17830541ec285a0
Artem Blokhin <rtmdragee@gmail.com>
2024-07-02 20:52:58 +0400

    Initial commit

---
+++ src/test/java/ru/netology/banklogin/page/DashboardPage.java
@@ -0,0 +1,18 @@
+package ru.netology.banklogin.page;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
+public class DashboardPage {
+    private SelenideElement heading = $("[data-test-id='dashboard']");
+
+    public void DashboardPageVisible() {
+        heading
+                .shouldBe(visible)
+                .shouldHave(Condition.text("Личный кабинет"));
+    }
+}
